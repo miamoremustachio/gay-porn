835697f0b5d90e3d2df51730f1b46ac36f614594
MiniMars1337 <miniakhmetov.marsel2002@yandex.ru>
2023-08-12 17:32:31 +0300

    добавил свой файл и свою ветку

---
+++ MarselMiniakhmetov/main.js
@@ -0,0 +1,17 @@
+function calc(operator, a, b) {
+    switch (operator) {
+        case 'add':
+            return a + b;
+        case 'multi':
+            return a * b;
+        case 'subtract':
+            return a - b;
+        default:
+            return 'Нет такой функции!';
+    }
+  }
+  
+  console.log(calc('add', 1, 2));
+  console.log(calc('multi', 1, 2));
+  console.log(calc('subtract', 3, 2));
+  console.log(calc('ERROR', NaN, NaN))
