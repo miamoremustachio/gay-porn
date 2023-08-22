469c0c94c468e60894ade5e3d064071a420f4ded
Alex13rus13 <sedoy.alex13rus@gmail.com>
2023-08-22 15:08:21 +0300

    Урок по циклам

---
+++ sedoykin/lesson-15.js
@@ -0,0 +1,16 @@
+let i = 1;
+while (i < 20) {
+console.log(i);
+i = i + 1;
+}
+
+let j = 1;
+do {
+    console.log(j);
+    j = j + 1;
+} while (j < 20);
+
+for ( let l = 1; l < 20; l++) {
+    console.log(l);
+} 
+
