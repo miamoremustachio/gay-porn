ce1c8ab9ebc727f63124b7121f5c60732b94ddd4
miamoremustachio <123443918+miamoremustachio@users.noreply.github.com>
2023-09-08 22:38:03 +0700

    add testing; fix bug in add() method

--- miamoremustachio/todo_arrays.js
+++ miamoremustachio/todo_arrays.js
@@ -118,7 +118,7 @@ const toDo = {
             checkValidity(isPriorityValid, priority);
             checkValidity(isTaskUnique, task);
         } catch (error) {
-            console.error(error.message);
+            return console.error(error.message);
         };
 
         const taskObject = getTaskObject(task, status, priority);
@@ -180,3 +180,28 @@ const toDo = {
             showAllTasksWith(STATUS.DONE);
     }
 };
+
+// testing:
+toDo.add(); // [invalid task]
+toDo.add('foo', 'bar'); // [incorrect status]
+toDo.add('foo', STATUS.TODO, 123); // [incorrect priority]
+toDo.add('foo'); // YAY!
+toDo.add('foo'); // [task exist]
+toDo.showList();
+
+toDo.changeStatus(); // [task not found]
+toDo.changeStatus('foo', undefined); // [incorrect status]
+toDo.changeStatus('foo', STATUS.DONE); // YAY!!
+toDo.showList();
+
+toDo.changePriority(null); // [task not found]
+toDo.changePriority('foo', NaN); // [incorrect priority]
+toDo.changePriority('foo', PRIORITY.HIGH); // YAY!!!
+toDo.showList();
+
+toDo.delete(); // delete last task
+toDo.delete('start'); // delete first task
+toDo.delete(1); // delete first task again
+toDo.delete('bar'); // [task not found]
+toDo.delete('get rid of 150,346 bookmarks in Chrome');
+toDo.showList(); // e m p t y . . .
