e51c88c8d30e66ceb26f4d8aa2732f64a25ea535
Alexey Klimavtsov <klimavtsov@gmail.com>
2023-09-18 20:25:43 +0300

    clean branch

--- danilmironov/getGender.js
+++
@@ -1,9 +0,0 @@
-const serverUrl = "https://api.genderize.io";
-
-const getGender = async (name) => {
-  return fetch(`${serverUrl}/?name=${name}`).then((response) => {
-    return response.json();
-  });
-};
-
-module.exports = getGender;
--- tasc8/main.mjs
+++
@@ -1,8 +0,0 @@
-import data from './data.json' assert {type: "json"};
-// console.log(data);
-// const json = JSON.stringify(data);
-const showFirstName = () => {
-    for (const firstName in data.users)
-    console.log(firstName)
-}
-showFirstName();
