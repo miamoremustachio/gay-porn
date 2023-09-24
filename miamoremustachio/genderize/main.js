54c89c63bdbdd6adc914173ea3c5391536867d7e
miamoremustachio <123443918+miamoremustachio@users.noreply.github.com>
2023-09-24 19:56:17 +0700

    add genderize
    rewrite the terminal version of Genderize to work on local server; add start message; move request() func in module file; feat: better name capitalization in response message

--- miamoremustachio/genderize/main.js
+++ miamoremustachio/genderize/main.js
@@ -1,50 +1,31 @@
 const {
-    API,
-    EXIT_COMMAND,
-    getResponseError,
+    START_MESSAGE,
     getNameError,
     getCapitalizedName,
-    readline,
+    request,
 } = require('./modules');
 
+const http = require('http');
+const server = http.createServer().listen(3000);
 
-function request(name) {
-    const url = `${API}?name=${name}`;
-    
-    return fetch(url)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(getResponseError(response));
+server.on('request', (req, res) => {
+    if (req.url === '/') {
+        return res.end(START_MESSAGE);
+    };
+
+    const nameFromUrl = req.url.slice(1);
+
+    request(nameFromUrl)
+    .then(data => {
+        const name = getCapitalizedName(data.name);
+        const gender = data.gender;
+        const probability = (Math.trunc(data.probability * 100));
+
+        if (!gender) {
+            throw new Error(getNameError(name));
         };
-
-        return response.json();
+        
+        res.end(`The name ${name} is ${gender} with a probability of ${probability}%`);
     })
-}
-
-function askName() {
-    readline.question("What is your name? ", name => {
-        if (name === EXIT_COMMAND) {
-            return readline.close();
-        };
-
-        request(name)
-        .then(data => {
-            const name = getCapitalizedName(data.name);
-            const gender = data.gender;
-            const probability = (Math.trunc(data.probability * 100));
-
-            if (!gender) {
-                throw new Error(getNameError(name));
-            };
-            
-            console.log(`The name ${name} is ${gender} with a probability of ${probability}%`);
-        })
-        .catch(error => console.error(error.message))
-        .finally(() => {
-            console.log(`* (type <${EXIT_COMMAND}> to exit)`);
-            askName();
-        });
-    });
-}
-
-askName();
+    .catch(error => res.end(error.message));
+});
--- miamoremustachio/genderize/modules.js
+++ miamoremustachio/genderize/modules.js
@@ -1,29 +1,35 @@
 const API = 'https://api.genderize.io';
 
-const EXIT_COMMAND = 'exit';
+const START_MESSAGE = "Welcome to Genderize!\nWrite any name in url and program will tell you if it's male or female.";
 
 function getResponseError(response) {
     return `${response.status}: ${response.statusText}`;
 }
 
 function getNameError(name) {
-    return `The name "${name}" wasn't found in database.`;
+    return `The name "${name}" wasn't found in database :(`;
 }
 
 function getCapitalizedName(name) {
-    return (name.at(0).toUpperCase() + name.slice(1));
+    return (name.at(0).toUpperCase() + name.slice(1).toLowerCase());
 }
 
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+function request(name) {
+    const url = `${API}?name=${name}`;
+    
+    return fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(getResponseError(response));
+            };
+
+            return response.json();
+    })
+}
 
 module.exports = {
-    API,
-    EXIT_COMMAND,
-    getResponseError,
+    START_MESSAGE,
     getNameError,
     getCapitalizedName,
-    readline,
+    request,
 };
