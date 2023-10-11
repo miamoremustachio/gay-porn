fd0490b560a2d8311309232429b8f5f20b70f7a8
miamoremustachio <123443918+miamoremustachio@users.noreply.github.com>
2023-10-11 20:41:06 +0700

    add weather data in response
    add date-fns package for date formatting; add response message with some data from city request, such as city name, current time, weather conditions etc.; feat: welcome and start messages

--- miamoremustachio/main.js
+++ miamoremustachio/main.js
@@ -1,5 +1,10 @@
 const { API_KEY } = require('./modules/api_key.js');
-const { HELLO_MESSAGE, START_MESSAGE } =require('./modules/constants.js');
+const { HELLO_MESSAGE, START_MESSAGE } = require('./modules/constants.js');
+
+const addMinutes = require('date-fns/addMinutes');
+const addSeconds = require('date-fns/addSeconds');
+const fromUnixTime = require('date-fns/fromUnixTime');
+const format = require('date-fns/format');
 
 const http = require('http');
 const server = http.createServer().listen(3000);
@@ -19,8 +24,28 @@ server.on('request', (req, res) => {
     geocodingRequest(cityName)
         .then(data => weatherRequest(data))
         .then(data => {
-            console.log(data);
-            res.end('YAY!');
+            const cityName = data.name;
+            const countryCode = data.sys.country;
+            const conditions = data.weather[0].description;
+
+            const localOffset = new Date().getTimezoneOffset(); // in minutes
+            const cityOffset = data.timezone; // in seconds
+
+            const UTCTime = addMinutes(new Date(), localOffset);
+            const cityTime = addSeconds(UTCTime, cityOffset);
+            const time = format(cityTime, 'MMM do, p'); // Jan 1st, 12:00 AM
+
+            const localSunrise = fromUnixTime(data.sys.sunrise);
+            const UTCSunrise = addMinutes(localSunrise, localOffset);
+            const citySunrise = addSeconds(UTCSunrise, cityOffset);
+            const sunrise = format(citySunrise, 'p'); // 12:00 AM
+
+            const localSunset = fromUnixTime(data.sys.sunset);
+            const UTCSunset = addMinutes(localSunset, localOffset);
+            const citySunset = addSeconds(UTCSunset, cityOffset);
+            const sunset = format(citySunset, 'p'); // 12:00 AM
+
+            res.end(`Weather conditions in ${cityName} (${countryCode}): ${conditions}; current time: ${time}; sunrise: ${sunrise}, sunset: ${sunset}.`);
         })
         .catch(error => res.end(error.message));
 });
