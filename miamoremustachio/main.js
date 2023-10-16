3b16bf14c8d7beda6190f6cb537b7b47473b0f9f
miamoremustachio <123443918+miamoremustachio@users.noreply.github.com>
2023-10-16 22:51:57 +0700

    refactor: create module for conditions-formatting func
    add extra_functions.js

--- miamoremustachio/main.js
+++ miamoremustachio/main.js
@@ -11,10 +11,7 @@ const {
     weatherRequest
 } = require('./modules/request_functions.js');
 
-const addMinutes = require('date-fns/addMinutes');
-const secondsToMinutes = require('date-fns/secondsToMinutes');
-const fromUnixTime = require('date-fns/fromUnixTime');
-const format = require('date-fns/format');
+const { getFormattedConditions } = require('./modules/extra_functions.js');
 
 const http = require('http');
 const server = http.createServer().listen(3000);
@@ -43,26 +40,8 @@ server.on('request', (req, res) => {
     geocodingRequest(cityName)
         .then(data => weatherRequest(data))
         .then(data => {
-            const cityName = data.name;
-            const countryCode = data.sys.country;
-            const conditions = data.weather[0].description;
-
-            const localOffset = new Date().getTimezoneOffset(); // in minutes
-            const cityOffset = secondsToMinutes(data.timezone); // in minutes
-            const totalOffset = localOffset + cityOffset;
-
-            const cityTime = addMinutes(new Date(), totalOffset);
-            const time = format(cityTime, 'MMM do, p'); // Jan 1st, 12:00 AM
-
-            const localSunrise = fromUnixTime(data.sys.sunrise);
-            const citySunrise = addMinutes(localSunrise, totalOffset);
-            const sunrise = format(citySunrise, 'p'); // 12:00 AM
-
-            const localSunset = fromUnixTime(data.sys.sunset);
-            const citySunset = addMinutes(localSunset, totalOffset);
-            const sunset = format(citySunset, 'p'); // 12:00 AM
-
-            res.end(`Weather conditions in ${cityName} (${countryCode}): ${conditions}; current time: ${time}; sunrise: ${sunrise}, sunset: ${sunset}.`);
+            const weatherConditionsString = getFormattedConditions(data);
+            res.end(weatherConditionsString);
         })
         .catch(error => res.end(error.message));
 });
---
+++ miamoremustachio/modules/extra_functions.js
@@ -0,0 +1,32 @@
+const addMinutes = require('date-fns/addMinutes');
+const secondsToMinutes = require('date-fns/secondsToMinutes');
+const fromUnixTime = require('date-fns/fromUnixTime');
+const format = require('date-fns/format');
+
+function getFormattedConditions(data) {
+    const cityName = data.name;
+    const countryCode = data.sys.country;
+    const conditions = data.weather[0].description;
+
+    const localOffset = new Date().getTimezoneOffset(); // in minutes
+    const cityOffset = secondsToMinutes(data.timezone); // in minutes
+    const totalOffset = localOffset + cityOffset;
+
+    const cityTime = addMinutes(new Date(), totalOffset);
+    const time = format(cityTime, 'MMM do, p'); // Jan 1st, 12:00 AM
+
+    const localSunrise = fromUnixTime(data.sys.sunrise);
+    const citySunrise = addMinutes(localSunrise, totalOffset);
+    const sunrise = format(citySunrise, 'p'); // 12:00 AM
+
+    const localSunset = fromUnixTime(data.sys.sunset);
+    const citySunset = addMinutes(localSunset, totalOffset);
+    const sunset = format(citySunset, 'p'); // 12:00 AM
+
+    return `Weather conditions in ${cityName} (${countryCode}): ${conditions};`
+    + ` current time: ${time};`
+    + ` sunrise: ${sunrise},`
+    + ` sunset: ${sunset}.`;
+}
+
+module.exports = { getFormattedConditions };
