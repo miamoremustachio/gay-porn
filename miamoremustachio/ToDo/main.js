134d3ab85c16219b1927930851c4ece3e2d6de57
miamoremustachio <123443918+miamoremustachio@users.noreply.github.com>
2023-11-10 20:33:31 +0700

    add CORS-handling middleware
    delete express_test

--- miamoremustachio/ToDo/main.js
+++ miamoremustachio/ToDo/main.js
@@ -14,8 +14,16 @@ const {
   SUCCESSFULLY_DELETED,
 } = INFO_MESSAGES;
 
+const setCrossOriginHeaders = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+};
+
 const app = express();
 
+app.use(setCrossOriginHeaders);
 app.use(express.json());
 
 app.get('/', (req, res) => {
