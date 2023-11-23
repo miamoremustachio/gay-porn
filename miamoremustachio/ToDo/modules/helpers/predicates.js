0815733893ab96fbbd7066961ab26868d8d0d9ca
miamoremustachio <123443918+miamoremustachio@users.noreply.github.com>
2023-11-23 13:13:33 +0700

    remove unused expressions and statements

--- miamoremustachio/ToDo/modules/helpers/predicates.js
+++
@@ -1,8 +0,0 @@
-const { ObjectId } = require('mongodb');
-
-async function isDocumentExists(collection, id) {
-  const document = await collection.findOne({ _id: new ObjectId(id) });
-  return document ? true : false;
-}
-
-module.exports = { isDocumentExists };
