84bb177ee724481d2fec51c9b7d69270b46b94e4
miamoremustachio <123443918+miamoremustachio@users.noreply.github.com>
2024-02-04 23:11:43 +0700

    refactor: middlewares tests
    remove duplicated test

--- miamoremustachio/ToDo/tests/middlewares.test.js
+++ miamoremustachio/ToDo/tests/middlewares.test.js
@@ -1,24 +1,16 @@
 const request = require('supertest');
-const mongoose = require('mongoose');
 
 const { app } = require('../app.js');
 const { database } = require('../modules/database/connection.js');
 
 const TASK_ID = '65633d765d1d06dcad8ff7d0';
-const RANDOM_ID = new mongoose.Types.ObjectId().toString();
 
 beforeAll(async () => {
   await database.connect();
 });
 
-describe('client errors', () => {
-  test('not found', async () => {
-    const response = await request(app)
-      .get(`/tasks/${RANDOM_ID}`);
-    expect(response.status).toBe(404);
-  });
-
-  test('unauthorized', async () => {
+describe('authorization', () => {
+  test('unauthorized user request', async () => {
     const response = await request(app)
       .get(`/tasks/${TASK_ID}`);
     expect(response.status).toBe(401);
