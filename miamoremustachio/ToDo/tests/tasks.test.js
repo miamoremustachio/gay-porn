670230082b67ff7d7f5452aa19de77f7551a875b
miamoremustachio <123443918+miamoremustachio@users.noreply.github.com>
2024-02-05 00:11:33 +0700

    refactor: tasks tests
    edit test data, HTTP headers and Jest matchers

--- miamoremustachio/ToDo/tests/tasks.test.js
+++ miamoremustachio/ToDo/tests/tasks.test.js
@@ -2,14 +2,12 @@ const request = require('supertest');
 
 const { app } = require('../app.js');
 const { database } = require('../modules/database/connection.js');
-const {
-  STATUSES,
-  PRIORITIES,
-} = require('../modules/helpers/constants.js');
+const { STATUSES, PRIORITIES } = require('../modules/helpers/constants.js');
 const { getDefaultDeadline } = require('../modules/helpers/time-helper.js');
 const { getFinalResourceId } = require('../modules/helpers/routes-helper.js');
 
-const TEST_USER_ID = '6561ff62413e98e914253b1f';
+const USER_ID = '6561ff62413e98e914253b1f';
+const HEADERS = { Authorization: USER_ID };
 
 const VALID_TASK = {
   'title': 'prepare for hard coding',
@@ -24,10 +22,10 @@ const VALID_FIELDS = {
 };
 
 const INVALID_TASK = {
-  'title': 'aa',
+  'title': 'a'.repeat(71),
   'priority': 'foo',
   'status': 'bar',
-  'deadline': '',
+  'deadline': Date.now(),
 };
 
 let taskId;
@@ -40,28 +38,28 @@ describe('tasks', () => {
   test('POST /tasks', async () => {
     const response = await request(app)
       .post('/tasks')
-      .set('Authorization', TEST_USER_ID)
+      .set(HEADERS)
       .send(VALID_TASK);
     taskId = getFinalResourceId(response.text);
-    expect(response.status).toEqual(201);
+    expect(response.status).toBe(201);
   });
 
   test('GET /tasks/:id', async () => {
     const response = await request(app)
       .get(`/tasks/${taskId}`)
-      .set('Authorization', TEST_USER_ID);
-    expect(response.status).toEqual(200);
+      .set(HEADERS);
+    expect(response.status).toBe(200);
     expect(response.body.title).toMatch(VALID_TASK.title);
     expect(response.body.priority).toMatch(VALID_TASK.priority);
-    expect(response.body.user._id).toMatch(TEST_USER_ID);
+    expect(response.body.user._id).toMatch(USER_ID);
   });
 
   test('PUT /tasks/:id', async () => {
     const response = await request(app)
       .put(`/tasks/${taskId}`)
-      .set('Authorization', TEST_USER_ID)
+      .set(HEADERS)
       .send(VALID_FIELDS);
-    expect(response.status).toEqual(200);
+    expect(response.status).toBe(200);
     expect(response.body.title).toMatch(VALID_FIELDS.title);
     expect(response.body.status).toMatch(VALID_FIELDS.status);
     expect(response.body.priority).toMatch(VALID_FIELDS.priority);
@@ -71,15 +69,15 @@ describe('tasks', () => {
   test('DELETE /tasks/:id', async () => {
     const response = await request(app)
       .delete(`/tasks/${taskId}`)
-      .set('Authorization', TEST_USER_ID);
-    expect(response.status).toEqual(204);
+      .set(HEADERS);
+    expect(response.status).toBe(204);
   });
 
   test('GET /tasks', async () => {
     const response = await request(app)
       .get('/tasks')
-      .set('Authorization', TEST_USER_ID);
-    expect(response.status).toEqual(200);
+      .set(HEADERS);
+    expect(response.status).toBe(200);
     for (const task of response.body) {
       expect(task._id).not.toMatch(taskId);
     }
@@ -90,9 +88,9 @@ describe('tasks (invalid)', () => {
   test('POST /tasks', async () => {
     const response = await request(app)
       .post('/tasks')
-      .set('Authorization', TEST_USER_ID)
+      .set(HEADERS)
       .send(INVALID_TASK);
-    expect(response.status).toEqual(400);
+    expect(response.status).toBe(400);
     console.log(`POST error message: \n${response.text}`);
   });
 });
@@ -101,8 +99,8 @@ describe('tasks (not found)', () => {
   test('GET /tasks', async () => {
     const response = await request(app)
       .get(`/tasks${taskId}`)
-      .set('Authorization', TEST_USER_ID);
-    expect(response.status).toEqual(404);
+      .set(HEADERS);
+    expect(response.status).toBe(404);
   });
 });
 
