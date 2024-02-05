79ddd7bd2c87067573b85cd81ba5974ec2b306f7
miamoremustachio <123443918+miamoremustachio@users.noreply.github.com>
2024-02-05 14:30:47 +0700

    test: authorization
    delete middlewares test

--- miamoremustachio/ToDo/tests/authorization.test.js
+++ miamoremustachio/ToDo/tests/authorization.test.js
@@ -1,19 +1,28 @@
 const request = require('supertest');
+const mongoose = require('mongoose');
 
 const { app } = require('../app.js');
 const { database } = require('../modules/database/connection.js');
 
 const TASK_ID = '65633d765d1d06dcad8ff7d0';
+const RANDOM_ID = new mongoose.Types.ObjectId().toString();
 
 beforeAll(async () => {
   await database.connect();
 });
 
-describe('authorization', () => {
-  test('unauthorized user request', async () => {
+describe('authorization errors', () => {
+  test('unauthorized', async () => {
     const response = await request(app)
       .get(`/tasks/${TASK_ID}`);
     expect(response.status).toBe(401);
+  });
+
+  test('forbidden', async () => {
+    const response = await request(app)
+      .get(`/tasks/${TASK_ID}`)
+      .set('Authorization', RANDOM_ID);
+    expect(response.status).toBe(403);
   });
 });
 
