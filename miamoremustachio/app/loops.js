ca6a8ac21fd0dc11a9627777c17f41ac18145d85
miamoremustachio <123443918+miamoremustachio@users.noreply.github.com>
2024-09-02 19:40:30 +0700

    add loops.js

---
+++ miamoremustachio/app/loops.js
@@ -0,0 +1,21 @@
+// "while" loop:
+let i = 1;
+
+while (i <= 19) {
+  console.log(i);
+  i++;
+}
+
+// "do...while" loop:
+let j = 1;
+
+do {
+  console.log(j);
+  j++;
+
+} while (j <= 19);
+
+// "for" loop:
+for (let k = 1; k <= 19; k++) {
+  console.log(k);
+}
