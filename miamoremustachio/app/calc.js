5f2d8f2e495018f422fa10965de2593449ddaf4b
miamoremustachio <123443918+miamoremustachio@users.noreply.github.com>
2024-09-03 12:39:55 +0700

    style: parentheses, blank lines

--- miamoremustachio/app/calc.js
+++ miamoremustachio/app/calc.js
@@ -19,13 +19,15 @@ function validate(a, b) {
 }
 
 function add(a, b) {
-  return (a + b);
+  return a + b;
 }
+
 function subtract(a, b) {
-  return (a - b);
+  return a - b;
 }
+
 function multiply(a, b) {
-  return (a * b);
+  return a * b;
 }
 
 function calc(operation, a, b) {
