abe7bff4fc8c2f261379590f3134e5f6ecfeff40
miamoremustachio <123443918+miamoremustachio@users.noreply.github.com>
2024-09-08 21:09:44 +0700

    refactor: rewrite math operations functions

--- miamoremustachio/app/calc.js
+++ miamoremustachio/app/calc.js
@@ -18,17 +18,9 @@ function validate(a, b) {
   }
 }
 
-function add(a, b) {
-  return a + b;
-}
-
-function subtract(a, b) {
-  return a - b;
-}
-
-function multiply(a, b) {
-  return a * b;
-}
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
 
 function calc(operation, a, b) {
   const validationPassed = validate(a, b);
